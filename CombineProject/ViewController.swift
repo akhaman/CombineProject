c30a7e1994d733ea3ffab0131af8cf0b23be2575
Ruslan <akhaman@yandex.ru>
2021-11-05 18:46:05 +0300

    [add] project

---
+++ CombineProject/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CombineProject
+//
+//  Created by Руслан Ахмадеев on 05.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
