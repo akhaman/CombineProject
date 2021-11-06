12b6ec54754030c01a395d8169d243c40b5d8027
Ruslan <akhaman@yandex.ru>
2021-11-06 22:55:44 +0300

    [update] navigation bar appearance

--- CombineProject/Components/NavigationBar.swift
+++ CombineProject/Components/NavigationBar.swift
@@ -11,10 +11,24 @@ class NavigationBar: UINavigationBar {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        prefersLargeTitles = true
+        setup()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Initial Configuration
+    
+    private func setup() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .init(red: 249/255, green: 249/255, blue: 249/255, alpha: 0.94)
+        
+        compactAppearance = appearance
+        standardAppearance = appearance
+        scrollEdgeAppearance = appearance
+        
+        prefersLargeTitles = true
+        isTranslucent = false
     }
 }
